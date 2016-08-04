4ee7764878abc6d723432feeafb29f7f5f297af9
Lucas Vieira <lucas@vieira.io>
2016-08-04 14:02:17 -0300

    fix zone files permission

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -12,6 +12,12 @@ copy_conf_file(){
   cp /data/named.conf.local /etc/bind/
 }
 
+fix_zone_files_permission() {
+  debug "fixing zone files permissions..."
+  chgrp bind "/data/db.*"
+  chmod g+rw "/data/db.*"
+}
+
 create_link_to_zone_files(){
   debug "creating symbolic link to the zone files..."
   for db_file in $(ls /data/db.*); do
@@ -33,8 +39,9 @@ start_named(){
 
 main(){
   copy_conf_file
+  fix_zone_files_permission
   create_link_to_zone_files
-fix_ddns_key_permission
+  fix_ddns_key_permission
   start_named
 }
 
