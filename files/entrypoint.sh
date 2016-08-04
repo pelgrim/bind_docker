8574766dc79c635e050fbae65c7ffff91a35e25f
Lucas Vieira <lucas@vieira.io>
2016-08-04 10:50:25 -0300

    link zone files inside bind cache dir

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -7,11 +7,27 @@ debug(){
   fi
 }
 
-main(){
+copy_conf_file(){
   debug "copying named.conf.local to bind conf dir..."
   cp /data/named.conf.local /etc/bind/
+}
+
+create_link_to_zone_files(){
+  debug "creating symbolic link to the zone files..."
+  for db_file in $(ls /data/db.*); do
+    ln -s $db_file /var/cache/bind/
+  done
+}
+
+start_named(){
   debug "starting named..."
   named -f -u bind
+}
+
+main(){
+  copy_conf_file
+  create_link_to_zone_files
+  start_named
 }
 
 main
